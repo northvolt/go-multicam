82226dc27958ed09c89ee17c46e147fec41b065c
Ron Evans <ron@hybridgroup.com>
2022-03-30 16:24:05 +0200

    channel: add basic implementation for create/delete (#3)

--- errors.go
+++ errors.go
@@ -3,7 +3,10 @@ package multicam
 import "errors"
 
 var (
-	ErrCannotOpenDriver = errors.New("cannot open driver")
-	ErrCannotSetParam   = errors.New("cannot set param")
-	ErrCannotGetParam   = errors.New("cannot get param")
+	ErrCannotOpenDriver    = errors.New("cannot open driver")
+	ErrCannotSetParam      = errors.New("cannot set param")
+	ErrCannotGetParam      = errors.New("cannot get param")
+	ErrInvalidChannel      = errors.New("invalid channel")
+	ErrCannotCreateChannel = errors.New("cannot create MultiCam channel")
+	ErrCannotDeleteChannel = errors.New("cannot delete MultiCam channel")
 )
