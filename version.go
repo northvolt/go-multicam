3e1b055cc1cf43c613b8ea5759198db3ef72217f
Ron Evans <ron@hybridgroup.com>
2022-10-20 15:58:35 +0200

    release: v0.4.0

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ import "os"
 
 // GoMulticamVersion of this package, for display purposes.
 // Change this variable on a new package release.
-const GoMulticamVersion = "0.3.0"
+const GoMulticamVersion = "0.4.0"
 
 // Version returns the current Golang package version.
 func Version() string {
